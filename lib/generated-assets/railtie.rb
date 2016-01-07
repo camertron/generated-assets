f3abc350099cd6aacfdb9266f6d06e9735288538
Cameron Dutro <camertron@gmail.com>
2016-01-07 12:20:49 -0800

    Remove check for app.config.assets.compile

--- lib/generated-assets/railtie.rb
+++ lib/generated-assets/railtie.rb
@@ -9,17 +9,13 @@ module GeneratedAssets
     end
 
     config.after_initialize do |app|
-      if app.config.assets.compile
-        app.config.assets.generated.apply!
-      end
+      app.config.assets.generated.apply!
     end
 
     initializer :i18n_js_assets, group: :all do |app|
-      if app.config.assets.compile
-        app.config.assets.paths << File.join(
-          GeneratedAssets.asset_dir, app.class.parent_name
-        )
-      end
+      app.config.assets.paths << File.join(
+        GeneratedAssets.asset_dir, app.class.parent_name
+      )
     end
   end
 end
